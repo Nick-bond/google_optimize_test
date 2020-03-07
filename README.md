880060f95d764607b2953a38c123fcfad826db7a
nick-bond <n.jsdevelop@gmail.com>
2020-03-07 16:13:05 +0200

    Add script which help to test google optimize in broowser in auto mode

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # google_optimize_test
-Test if Google Optimize set and work in browser
+
+Testing if Google Optimize was correct to set and its work in the browser
+
+### Variables
+
+- `URL` - website url for test
+- `Experiment_Id` - id of google optimize experiment
+- `Global_variable` - global variable name
+
+### For run script
+- Clone project
+- Install all dependencies
+```
+npm install
+```
+- Set all variable with corresponding data to your experiment
+- Open dev tools
+- Run in console
+``` 
+node go-test.js
+```
---
+++ go-test.js
@@ -0,0 +1,29 @@
+const puppeteer = require('puppeteer');
+const URL = <URL>
+let result = 1;
+
+const testEnvForExper = () => {
+  if (result === 20) {
+    clearInterval(timer);
+    return;
+  }
+
+  (async () => {
+    const browser = await puppeteer.launch();
+    const context = await browser.createIncognitoBrowserContext();
+    const page = await context.newPage();
+    await page.goto(URL);
+    const status = await page.evaluate(() => {
+      return {
+        go_status: window.google_optimize.get(<Experiment_Id>),
+        window_global_var_status: window.<Global_variable>,
+      };
+    });
+    console.log(status)
+    await browser.close();
+  })();
+
+  result += 1;
+};
+
+const timer = setInterval(testEnvForExper, 1000);
